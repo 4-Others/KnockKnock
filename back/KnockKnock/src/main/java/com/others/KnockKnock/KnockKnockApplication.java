8e4351f5cbad06cfd0d358fe530332cf815fe41d
gimtaehyeong <adsds126@gmail.com>
2023-05-21 22:33:49 +0900

    first commit

---
+++ back/KnockKnock/src/main/java/com/others/KnockKnock/KnockKnockApplication.java
@@ -0,0 +1,13 @@
+package com.others.KnockKnock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnockKnockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnockKnockApplication.class, args);
+	}
+
+}
---
+++ back/KnockKnock/src/test/java/com/others/KnockKnock/KnockKnockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.others.KnockKnock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnockKnockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
