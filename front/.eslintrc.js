d249fee0da284e46f2244935bc901bd078aa4b10
BangBang-e <youngjae7133@gmail.com>
2023-05-30 01:18:15 +0900

    프론트 초기세팅

---
+++ front/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  overrides: [
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'semi': [
+      'error',
+      'always'
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
