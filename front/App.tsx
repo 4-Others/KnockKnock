b2ab5efa7ba028dca6f838f886628dab2e27a256
MODAC0 <modac0302@gmail.com>
2023-05-31 18:30:32 +0900

    gradient success

--- front/App.tsx
+++ front/App.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import {View, Text, StyleSheet} from 'react-native';
+import {variables} from './src/style/variables';
+import LinearGradient from 'react-native-linear-gradient';
 
-const App = (): JSX.Element => {
+const App = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontFamily: 'Pretendard-Bold', fontSize: 20}}>ㅎㅇㅎㅇ</Text>
+    <View style={Style.layout}>
+      <LinearGradient
+        style={Style.linearGradient}
+        start={{x: 0, y: 0}}
+        end={{x: 0.6, y: 0.6}}
+        colors={['#FEA97A', '#FF5789']}
+      />
+      <Text style={Style.title}>Sign in with Facebook</Text>
+      <Text style={Style.title}>ㅎㅇㅎㅇ</Text>
     </View>
   );
 };
 
 export default App;
+
+const Style = StyleSheet.create({
+  title: {
+    fontSize: 40,
+    fontFamily: 'Pretendard-Black',
+    color: variables.main,
+  },
+  layout: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  linearGradient: {
+    flex: 1,
+    paddingLeft: 200,
+    paddingRight: 200,
+    borderRadius: 5,
+  },
+});
---
+++ front/src/style/variables.tsx
@@ -0,0 +1,47 @@
+export const variables = {
+  main: '#ff5789',
+  // 텍스트 컬러 진한 순
+  text_1: '#1b1b1b',
+  text_2: '#333333',
+  text_3: '#666666',
+  text_4: '#999999',
+  text_5: '#aaaaaa',
+  text_6: '#cccccc',
+  // 라인 컬러 진한 순
+  line_1: '#dddddd',
+  line_2: '#eeeeee',
+  // 아이콘 컬러 진한 순
+  icon_1: '#ff5789',
+  icon_2: '#aaaaaa',
+  icon_3: '#cccccc',
+  icon_4: '#d6d6d6',
+  // 유효성 컬러
+  error: '#ee5838',
+  // 보드 컬러 팔레트
+  board_1: '#a6c3cb',
+  board_2: '#006f83',
+  board_3: '#03a180',
+  board_4: '#7faed0',
+  board_5: '#0165a1',
+  board_6: '#f7ce01',
+  board_7: '#ee9421',
+  board_8: '#e75838',
+  board_9: '#444065',
+  board_10: '#be3b62',
+  // 보드 머티리얼 컬러
+  Mater_1: '#c63360',
+  Mater_2: '#822da4',
+  Mater_3: '#5837ac',
+  Mater_4: '#3c49a5',
+  Mater_5: '#4386de',
+  Mater_6: '#4599dd',
+  Mater_7: '#4da9be',
+  Mater_8: '#5e9e52',
+  Mater_9: '#89b254',
+  Mater_10: '#c2ca51',
+  Mater_11: '#f6d959',
+  Mater_12: '#f4b63f',
+  Mater_13: '#ec9234',
+  Mater_14: '#e25d34',
+  Mater_15: '#757575',
+};
