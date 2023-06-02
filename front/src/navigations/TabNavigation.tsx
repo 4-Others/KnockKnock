c5706248e377e32c6705bd294109d7d7db2b1d0d
BangBang-e <youngjae7133@gmail.com>
2023-06-02 15:59:38 +0900

    bottomTab 네이게이터 구현 중

--- front/src/navigations/TabNavigation.tsx
+++ front/src/navigations/TabNavigation.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {StyleSheet} from 'react-native';
+import {variables} from '../style/variables';
+import Icon from 'react-native-vector-icons/Ionicons';
 import ListBoard from '../pages/listBoard/ListBoard';
 import Calendar from '../pages/calendar/Calendar';
 import ListAdd from '../pages/listAdd/ListAdd';
@@ -9,17 +12,51 @@ import Notifications from '../pages/notifications/Notifications';
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
+  Icon.loadFont();
   return (
     <Tab.Navigator
-      screenOptions={({route}) => ({headerShown: false})}
-      initialRouteName="List Board">
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
+
+          if (route.name === 'List Board') {
+            iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+          } else if (route.name === 'Calendar') {
+            iconName = focused ? 'ios-list' : 'ios-list-outline';
+          } else if (route.name === 'Add') {
+            iconName = focused ? 'ios-list' : 'ios-list-outline';
+          } else if (route.name === 'Search') {
+            iconName = focused ? 'ios-list' : 'ios-list-outline';
+          } else if (route.name === 'Notifications') {
+            iconName = focused ? 'ios-list' : 'ios-list-outline';
+          }
+
+          return <Icon name={`${iconName}`} size={30} color={'#aaaaaa'} />;
+        },
+        tabBarActiveTintColor: '#ff5789',
+        tabBarInactiveTintColor: '#aaaaaa',
+        tabBarShowLabel: false,
+      })}>
       <Tab.Screen name="List Board" component={ListBoard} />
       <Tab.Screen name="Calendar" component={Calendar} />
       <Tab.Screen name="Add" component={ListAdd} />
       <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Notifications" component={Notifications} />
+      <Tab.Screen name="Notifications" component={Notifications} options={{tabBarBadge: 3}} />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'Pretendard-Medium',
+    color: variables.main,
+    fontSize: 30,
+  },
+});
 
 export default TabNavigation;
