a2eed9d4aeab9814f281e5b94bd8c9b9b54754c4
MODAC0 <modac0302@gmail.com>
2023-06-13 18:07:38 +0900

    ✅ ios setting & package.json 설정 수정

--- front/App.tsx
+++ front/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import MainNavigation from './src/navigations/MainNavigation';
-
 const App = () => {
   return (
     <View style={styles.container}>
