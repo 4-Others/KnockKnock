8b53fd1da725528f5e4a850404c8792fdd0be7e2
MODAC0 <modac0302@gmail.com>
2023-07-04 17:29:07 +0900

    react-native-gesture-handler 버전 변경 및 bottomsheet test 적용

---
+++ front/src/components/BottomSheet.tsx
@@ -0,0 +1,103 @@
+import React, {useEffect, useRef} from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Modal,
+  Animated,
+  TouchableWithoutFeedback,
+  Dimensions,
+  PanResponder,
+} from 'react-native';
+
+type BottomSheetProps = {
+  modalVisible: boolean;
+  setModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const BottomSheet: React.FC<BottomSheetProps> = props => {
+  const {modalVisible, setModalVisible} = props;
+  const screenHeight = Dimensions.get('screen').height;
+  const panY = useRef(new Animated.Value(screenHeight)).current;
+  const translateY = panY.interpolate({
+    inputRange: [-1, 0, 1],
+    outputRange: [0, 0, 1],
+  });
+
+  const resetBottomSheet = Animated.timing(panY, {
+    toValue: 0,
+    duration: 300,
+    useNativeDriver: true,
+  });
+
+  const closeBottomSheet = Animated.timing(panY, {
+    toValue: screenHeight,
+    duration: 300,
+    useNativeDriver: true,
+  });
+
+  const panResponders = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => false,
+      onPanResponderMove: (event, gestureState) => {
+        panY.setValue(gestureState.dy);
+      },
+      onPanResponderRelease: (event, gestureState) => {
+        if (gestureState.dy > 0 && gestureState.vy > 1.5) {
+          closeModal();
+        } else {
+          resetBottomSheet.start();
+        }
+      },
+    }),
+  ).current;
+
+  useEffect(() => {
+    if (props.modalVisible) {
+      resetBottomSheet.start();
+    }
+  }, [props.modalVisible]);
+
+  const closeModal = () => {
+    closeBottomSheet.start(() => {
+      setModalVisible(false);
+    });
+  };
+
+  return (
+    <Modal visible={modalVisible} animationType={'fade'} transparent statusBarTranslucent>
+      <View style={styles.overlay}>
+        <TouchableWithoutFeedback onPress={closeModal}>
+          <View style={styles.background} />
+        </TouchableWithoutFeedback>
+        <Animated.View
+          style={{...styles.bottomSheetContainer, transform: [{translateY: translateY}]}}
+          {...panResponders.panHandlers}>
+          <Text>This is BottomSheet</Text>
+        </Animated.View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  },
+  background: {
+    flex: 1,
+  },
+  bottomSheetContainer: {
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+});
+
+export default BottomSheet;
