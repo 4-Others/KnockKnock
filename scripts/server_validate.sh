e0fd034f3e68de4919d161751271cf17e43fedf2
gimtaehyeong <adsds126@gmail.com>
2023-08-02 18:59:52 +0900

    last final modify script

--- scripts/server_validate.sh
+++ scripts/server_validate.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Docker 서비스가 응답하는지 확인합니다.
+
+APP_ENV_FILE="/home/ssm-user/app.env"
+
 # 여기서 ACTIVE_PROFILE과 ACTIVE_PORT는 여러분의 프로젝트에 맞게 수정해야 합니다.
 ROOT_PATH="/home/ssm-user/KnockKnock/back/KnockKnock"  # 애플리케이션 루트 경로 설정
-ACTIVE_PROFILE=$(cat $/home/ssm-user/app.env | grep "^ACTIVE_PROFILE" | cut -d "=" -f2)  # app.env 파일에서 ACTIVE_PROFILE 읽어오기
-ACTIVE_PORT=$(cat $/home/ssm-user/app.env | grep "^ACTIVE_PORT" | cut -d "=" -f2)  # app.env 파일에서 ACTIVE_PORT 읽어오기
-
+ACTIVE_PROFILE=$(grep "ACTIVE_PROFILE" "$APP_ENV_FILE" | cut -d '=' -f2)  # app.env 파일에서 ACTIVE_PROFILE 읽어오기
+ACTIVE_PORT=$(grep "ACTIVE_PORT" "$APP_ENV_FILE" | cut -d '=' -f2)  # app.env 파일에서 ACTIVE_PORT 읽어오기
 # Active profile과 port가 설정되었는지 확인
 if [ -z "$ACTIVE_PROFILE" ] || [ -z "$ACTIVE_PORT" ]; then
   echo "ERROR: Active profile or port is not set in app.env file."
