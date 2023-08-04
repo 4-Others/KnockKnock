426d7bf843e5affb5163fbd713d855a7f494e175
MODAC0 <modac0302@gmail.com>
2023-08-04 20:22:40 +0900

    회원가입 & 로그인 로직 추가 && 스플래시 화면 추가

--- front/src/pages/signUp/SignUpTab.tsx
+++ front/src/pages/signUp/SignUpTab.tsx
@@ -1,49 +1,32 @@
-import React, {useState, useEffect} from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import {onLoginProps} from '../../navigations/StackNavigator';
-import SignAgree from './SignAgree';
-import SignEmail from './SignEmail';
-import SignPassword from './SignPassword';
-import SignSuccess from './SignSuccess';
+import {SignUserInfo, SignEmailAuth, SignAgree, SignSuccess} from './SignScreen';
 import Header from '../../components/Header';
 import {SafeAreaView} from 'react-native';
 import {View, StyleSheet} from 'react-native';
-import * as Progress from 'react-native-progress';
 import Config from 'react-native-config';
+import React from 'react';
+import {AuthProps} from '../../navigations/StackNavigator';
 
 const Tab = createMaterialTopTabNavigator();
 
-const SignUpTab: React.FC<onLoginProps> = ({onLogin}) => {
-  const [deps, setDeps] = useState(0.25);
-
+const SignUpTab: React.FC<AuthProps> = () => {
   const url = Config.API_APP_KEY;
-
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Header title="회원가입" type="signUp" />
-      <View style={styles.progressBar}>
-        <Progress.Bar
-          progress={deps}
-          width={null}
-          height={2}
-          color={'#1b1b1b'}
-          unfilledColor={'#eee'}
-          borderColor={'#fff'}
-        />
-      </View>
-      <Tab.Navigator tabBar={props => <></>}>
-        <Tab.Screen name="SignAgree">
-          {props => <SignAgree {...props} setDeps={setDeps} />}
+      <View style={styles.progressBar}></View>
+      <Tab.Navigator
+        tabBar={props => <Header {...props} title="회원가입" type="signUp" />}
+        screenOptions={{
+          swipeEnabled: false,
+        }}>
+        <Tab.Screen name="SignAgree">{props => <SignAgree {...props} />}</Tab.Screen>
+        <Tab.Screen name="SignUserInfo" initialParams={{password: ''}}>
+          {props => <SignUserInfo {...props} url={url} />}
         </Tab.Screen>
-        <Tab.Screen name="SignPassword" initialParams={{password: ''}}>
-          {props => <SignPassword {...props} setDeps={setDeps} url={url} />}
+        <Tab.Screen name="SignEmailAuth">
+          {props => <SignEmailAuth {...props} url={url} />}
         </Tab.Screen>
-        <Tab.Screen name="SignEmail">
-          {props => <SignEmail {...props} setDeps={setDeps} url={url} />}
-        </Tab.Screen>
-        <Tab.Screen name="SignSuccess">
-          {props => <SignSuccess {...props} onLogin={onLogin} />}
-        </Tab.Screen>
+        <Tab.Screen name="SignSuccess">{props => <SignSuccess {...props} />}</Tab.Screen>
       </Tab.Navigator>
     </SafeAreaView>
   );
