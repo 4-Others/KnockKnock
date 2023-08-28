2b9740fe53cdcc30d4e923178409c5608bf85e20
gimtaehyeong <adsds126@gmail.com>
2023-08-28 18:38:41 +0900

    modify userrepository

--- back/KnockKnock/src/main/java/com/others/KnockKnock/domain/user/repository/UserRepository.java
+++ back/KnockKnock/src/main/java/com/others/KnockKnock/domain/user/repository/UserRepository.java
@@ -14,9 +14,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findById(String id);
-
     Optional<User> findByUserId(Long userId);
-    List<User> findByLastLoggedInBeforeAndStatus(LocalDateTime lastLoggedIn, Status status);
-
-    List<User> findByLastLoggedInBefore(LocalDateTime oneYearAgo);
 }
