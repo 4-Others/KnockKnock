56d3a4ef62833f5f92ce04b5d97a07efb4924b3d
jw_0819 <jw_0819@naver.com>
2023-08-29 22:22:56 +0900

    :bug: 더미 데이터 수정

--- back/KnockKnock/src/main/java/com/others/KnockKnock/domain/dummydata/DummyDataLoader.java
+++ back/KnockKnock/src/main/java/com/others/KnockKnock/domain/dummydata/DummyDataLoader.java
@@ -6,10 +6,12 @@ import com.others.KnockKnock.domain.schedule.entity.Schedule;
 import com.others.KnockKnock.domain.schedule.repository.ScheduleRepository;
 import com.others.KnockKnock.domain.user.entity.User;
 import com.others.KnockKnock.domain.user.repository.UserRepository;
+import com.others.KnockKnock.security.oauth.entity.ProviderType;
+import com.others.KnockKnock.security.oauth.entity.RoleType;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-//import org.springframework.transaction.annotation.Transactional;
+// import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -34,75 +36,79 @@ public class DummyDataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         String encryptedPassword1 = passwordEncoder.encode("ADSDSDS12!!");
-        LocalDate birth1 = LocalDate.of(1996,12,13);
+        LocalDate birth1 = LocalDate.of(1996, 12, 13);
         User user1 = User.builder()
-                .id("tester123")
-                .username("tester")
-                .email("john@example.com")
-                .password(encryptedPassword1)
-                .emailVerifiedYn("Y")
-                .birth(birth1)
-                .pushAgree(true)
-                .createdAt(LocalDateTime.now())
-                .modifiedAt(LocalDateTime.now())
-                .build();
+                         .id("tester123")
+                         .username("tester")
+                         .email("john@example.com")
+                         .password(encryptedPassword1)
+                         .emailVerifiedYn("Y")
+                         .birth(birth1)
+                         .pushAgree(true)
+                         .createdAt(LocalDateTime.now())
+                         .modifiedAt(LocalDateTime.now())
+                         .roleType(RoleType.USER)
+                         .providerType(ProviderType.LOCAL)
+                         .build();
         userRepository.save(user1);
 
         String encryptedPassword2 = passwordEncoder.encode("ADSDS126@@#");
-        LocalDate birth2 = LocalDate.of(1994,06,17);
+        LocalDate birth2 = LocalDate.of(1994, 06, 17);
         User user2 = User.builder()
-                .id("tester1234")
-                .username("tester")
-                .email("thisissample@gmail.com")
-                .password(encryptedPassword2)
-                .emailVerifiedYn("Y")
-                .birth(birth2)
-                .pushAgree(true)
-                .createdAt(LocalDateTime.now())
-                .modifiedAt(LocalDateTime.now())
-                .build();
+                         .id("tester1234")
+                         .username("tester")
+                         .email("thisissample@gmail.com")
+                         .password(encryptedPassword2)
+                         .emailVerifiedYn("Y")
+                         .birth(birth2)
+                         .pushAgree(true)
+                         .createdAt(LocalDateTime.now())
+                         .modifiedAt(LocalDateTime.now())
+                         .roleType(RoleType.USER)
+                         .providerType(ProviderType.LOCAL)
+                         .build();
         userRepository.save(user2);
 
         Schedule schedule1 = Schedule.builder().title("Meeting")
-                .content("this is schedule1")
-                .period(Schedule.Period.SPECIFIC_TIME)
-                .startAt("2023-08-07 12:00")
-                .endAt("2023-08-07 13:00")
-                .complete(false)
-                .alerts(Arrays.asList(15, 30, 60))
-                .user(user1)
-                .build();
+                                 .content("this is schedule1")
+                                 .period(Schedule.Period.SPECIFIC_TIME)
+                                 .startAt("2023-08-07 12:00")
+                                 .endAt("2023-08-07 13:00")
+                                 .complete(false)
+                                 .alerts(Arrays.asList(15, 30, 60))
+                                 .user(user1)
+                                 .build();
         scheduleRepository.save(schedule1);
 
         Schedule schedule2 = Schedule.builder()
-                .content("this is schedule2")
-                .period(Schedule.Period.SPECIFIC_TIME)
-                .startAt("2023-08-07 10:00")
-                .endAt("2023-08-07 11:00")
-                .complete(false)
-                .alerts(Arrays.asList(15, 30, 60))
-                .user(user2)
-                .build();
+                                 .content("this is schedule2")
+                                 .period(Schedule.Period.SPECIFIC_TIME)
+                                 .startAt("2023-08-07 10:00")
+                                 .endAt("2023-08-07 11:00")
+                                 .complete(false)
+                                 .alerts(Arrays.asList(15, 30, 60))
+                                 .user(user2)
+                                 .build();
         scheduleRepository.save(schedule2);
 
         Notification notification1 = Notification.builder()
-                .title("WorkOut")
-                .notifyAt("2023-08-07 09:45")
-                .delivered(false)
-                .isRead(false)
-                .schedule(schedule1)
-                .user(user1)
-                .build();
+                                         .title("WorkOut")
+                                         .notifyAt("2023-08-07 09:45")
+                                         .delivered(false)
+                                         .isRead(false)
+                                         .schedule(schedule1)
+                                         .user(user1)
+                                         .build();
         notificationRepository.save(notification1);
 
         Notification notification2 = Notification.builder()
-                .title("Study")
-                .notifyAt("2023-08-07 09:45")
-                .delivered(false)
-                .isRead(false)
-                .schedule(schedule2)
-                .user(user2)
-                .build();
+                                         .title("Study")
+                                         .notifyAt("2023-08-07 09:45")
+                                         .delivered(false)
+                                         .isRead(false)
+                                         .schedule(schedule2)
+                                         .user(user2)
+                                         .build();
         notificationRepository.save(notification2);
     }
 }
