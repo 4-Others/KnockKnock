8b7e338b4c893ae79fe6ef873b164bbe3712465c
gimtaehyeong <adsds126@gmail.com>
2023-09-14 10:28:03 +0900

    ddlauto update

--- back/KnockKnock/src/main/java/com/others/KnockKnock/domain/dummydata/DummyDataLoader.java
+++ back/KnockKnock/src/main/java/com/others/KnockKnock/domain/dummydata/DummyDataLoader.java
@@ -5,6 +5,7 @@ import com.others.KnockKnock.domain.notification.repository.NotificationReposito
 import com.others.KnockKnock.domain.schedule.entity.Schedule;
 import com.others.KnockKnock.domain.schedule.repository.ScheduleRepository;
 import com.others.KnockKnock.domain.tag.entity.Tag;
+import com.others.KnockKnock.domain.tag.repository.TagRepository;
 import com.others.KnockKnock.domain.user.entity.User;
 import com.others.KnockKnock.domain.user.repository.UserRepository;
 import com.others.KnockKnock.security.oauth.entity.ProviderType;
@@ -17,19 +18,22 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
-@Component
+//@Component
 public class DummyDataLoader implements CommandLineRunner {
 
     private final ScheduleRepository scheduleRepository;
     private final NotificationRepository notificationRepository;
     private final UserRepository userRepository;
+    private final TagRepository tagRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public DummyDataLoader(ScheduleRepository scheduleRepository, NotificationRepository notificationRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public DummyDataLoader(ScheduleRepository scheduleRepository, NotificationRepository notificationRepository, UserRepository userRepository, TagRepository tagRepository, PasswordEncoder passwordEncoder) {
         this.scheduleRepository = scheduleRepository;
         this.notificationRepository = notificationRepository;
         this.userRepository = userRepository;
+        this.tagRepository = tagRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -70,6 +74,25 @@ public class DummyDataLoader implements CommandLineRunner {
                          .build();
         userRepository.save(user2);
 
+        Tag tag1 = Tag.builder()
+                .name("Tag1")
+                .color("red")
+                .build();
+        tagRepository.save(tag1);
+
+        Tag tag2 = Tag.builder()
+                .name("Tag2")
+                .color("yellow")
+                .build();
+        tagRepository.save(tag2);
+
+        Tag tag3 = Tag.builder()
+                .name("Tag3")
+                .color("blue")
+                .build();
+        tagRepository.save(tag3);
+
+
         Schedule schedule1 = Schedule.builder().title("Meeting")
                                  .content("this is schedule1")
                                  .period(Schedule.Period.SPECIFIC_TIME)
@@ -78,7 +101,7 @@ public class DummyDataLoader implements CommandLineRunner {
                                  .complete(false)
                                  .alerts(Arrays.asList(15, 30, 60))
                                  .user(user1)
-                                 .tag(Collections.singletonList(new Tag()))
+                                 .tag(Arrays.asList(tag1,tag2))
                                  .build();
         scheduleRepository.save(schedule1);
 
@@ -90,7 +113,7 @@ public class DummyDataLoader implements CommandLineRunner {
                                  .complete(false)
                                  .alerts(Arrays.asList(15, 30, 60))
                                  .user(user2)
-                                 .tag(Collections.singletonList(new Tag()))
+                                 .tag(Arrays.asList(tag3))
                                  .build();
         scheduleRepository.save(schedule2);
 
