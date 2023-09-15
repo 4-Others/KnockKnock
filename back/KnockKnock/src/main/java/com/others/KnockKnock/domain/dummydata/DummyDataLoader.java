aa549a6492b9ae7f63f726ad05759ec926b1cb23
gimtaehyeong <adsds126@gmail.com>
2023-09-15 19:13:28 +0900

    dummy joosuck

--- back/KnockKnock/src/main/java/com/others/KnockKnock/domain/dummydata/DummyDataLoader.java
+++ back/KnockKnock/src/main/java/com/others/KnockKnock/domain/dummydata/DummyDataLoader.java
@@ -20,7 +20,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@Component
+//@Component
 public class DummyDataLoader implements CommandLineRunner {
 
     private final ScheduleRepository scheduleRepository;
