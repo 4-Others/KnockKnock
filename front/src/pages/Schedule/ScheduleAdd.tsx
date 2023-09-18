1700e4458bb6c69983519fb062375fe269a7ec5e
MODAC0 <modac0302@gmail.com>
2023-09-18 11:42:47 +0900

    ♻️ 보드 선택 팝업 변경

--- front/src/pages/Schedule/ScheduleAdd.tsx
+++ front/src/pages/Schedule/ScheduleAdd.tsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
-import {StyleSheet, SafeAreaView, Alert} from 'react-native';
+import {StyleSheet, SafeAreaView} from 'react-native';
 import axios from 'axios';
-import {variables} from '../../style/variables';
 import {useDispatch, useSelector} from 'react-redux';
 import {addScheduleItem} from '../../util/redux/scheduleSlice';
 import {AuthProps} from '../../navigations/StackNavigator';
@@ -28,7 +27,6 @@ const ScheduleAdd: React.FC<AuthProps> = () => {
       color: '',
     },
   };
-  console.log(user);
   const [scheduleWillAdd, setScheduleWillAdd] = useState(data);
   const handleAddSchedule = async () => {
     if (url) {
@@ -49,54 +47,10 @@ const ScheduleAdd: React.FC<AuthProps> = () => {
   };
 
   return (
-    <SafeAreaView style={scheduleOptionStyles.container}>
+    <SafeAreaView style={{flex: 1}}>
       <Header title="스케줄 등록" nextFunc={handleAddSchedule} />
       <ScheduleOption scheduleWillAdd={scheduleWillAdd} setScheduleWillAdd={setScheduleWillAdd} />
     </SafeAreaView>
   );
 };
 export default ScheduleAdd;
-
-export const scheduleOptionStyles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  contentLayout: {
-    marginRight: 24,
-    marginLeft: 24,
-    marginTop: 24,
-  },
-  contentTitleInput: {
-    fontFamily: variables.font_3,
-    color: variables.text_2,
-    fontSize: 14,
-    borderBottomWidth: 1,
-    borderBottomColor: variables.line_1,
-    paddingBottom: 16,
-  },
-  icon: {
-    width: 24,
-    height: 24,
-  },
-  contentInput: {
-    width: '100%',
-    marginTop: 20,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-  },
-  iconContainer: {
-    marginRight: 30,
-  },
-  inputContainer: {
-    flex: 1,
-    borderBottomWidth: 1,
-    borderBottomColor: variables.line_1,
-    paddingBottom: 16,
-  },
-  inputTitle: {
-    fontFamily: variables.font_3,
-    color: variables.text_2,
-    fontSize: 14,
-  },
-});
