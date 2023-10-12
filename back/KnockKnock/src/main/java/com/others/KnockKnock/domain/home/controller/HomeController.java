765a146f500993847b05cd8ec7d385801d6fdbf5
gimtaehyeong <adsds126@gmail.com>
2023-10-12 16:53:11 +0900

    home api address change

--- back/KnockKnock/src/main/java/com/others/KnockKnock/domain/home/controller/HomeController.java
+++ back/KnockKnock/src/main/java/com/others/KnockKnock/domain/home/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/home")
+@RequestMapping("/")
 public class HomeController {
     @GetMapping
     public ResponseEntity<?> getHome() {
--- back/KnockKnock/src/main/java/com/others/KnockKnock/security/config/SecurityConfig.java
+++ back/KnockKnock/src/main/java/com/others/KnockKnock/security/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .authorizeRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                .antMatchers("/home").permitAll()
+                .antMatchers("/").permitAll()
                 .antMatchers("/api/v1/emails/**").permitAll()
                 .antMatchers("/api/v1/auth/login").permitAll()
                 .antMatchers("/api/v1/users", "/api/v1/users/signup", "/api/v1/users/me/**").permitAll()
